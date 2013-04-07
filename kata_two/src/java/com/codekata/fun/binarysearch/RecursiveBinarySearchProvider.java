a120b3c0ec8f7fdc30124f275719d3878382e156
Andrei Dan <adan@smallfootprint.com>
2013-04-07 17:30:11 +0300

    Classes for recursive implementation.

---
+++ kata_two/src/java/com/codekata/fun/binarysearch/RecursiveBinarySearchProvider.java
@@ -0,0 +1,14 @@
+package com.codekata.fun.binarysearch;
+
+/**
+ * Author: andrei
+ * Date: 4/7/13
+ */
+public class RecursiveBinarySearchProvider implements IBinarySearchProvider {
+
+    private static final int NOT_FOUND_INDEX = -1;
+
+    public int chop(int value, int[] sortedArray) {
+        return NOT_FOUND_INDEX;
+    }
+}
